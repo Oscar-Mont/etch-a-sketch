ee38f6d05825723c1b184b991c956e3d7f722a9f
Oscar Monteverde <108705978+Oscar-Mont@users.noreply.github.com>
2024-06-09 16:18:16 -0600

    Add start and reset button and functions for them.
    There is now a start button that activates a
    function that creates a grid with the number
    specified by an input variable.
    
    There is a reset button that for now only reactivates
    the start button to play another game.
    
    Reset button lacks complete funcionality.
    
    Basic CSS was added to make website more easy to
    understand while working with the JS code logic.

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-//grid
+//creating the grid based on specified number
 
-const container = document.querySelector(".grid");
+const inputNumber = document.querySelector("input");
+const grid = document.querySelector(".grid");
+const startBtn = document.querySelector(".start-button")
+const resetBtn = document.querySelector(".reset-button");
 
-function grid(numOFDivs) {
-
+function createGridItems() {
+    let numOfDivs = inputNumber.value * inputNumber.value;
+    for (let i = 0; i < numOfDivs; i++) {
+        const gridSquare = document.createElement("div");
+        gridSquare.classList.add("grid-square");
+        grid.appendChild(gridSquare);
+    }
 }
 
-//hover button
+startBtn.addEventListener("click", () => {
+    createGridItems();
+    startBtn.disabled = true;
+})
 
+resetBtn.addEventListener("click", () => {
+    //pending to add functionality that resets the grid
+    startBtn.disabled = false;
+})
