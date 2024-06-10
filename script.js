bdfb1f222c083d12a116ca29ef2e795e22f5d134
Oscar Monteverde <108705978+Oscar-Mont@users.noreply.github.com>
2024-06-09 18:21:34 -0600

    Modify Grid Creation Approach and CSS flexbox.
    The previous way to make a grid had a defect
    consisting of the cells not being evenly distributed.
    That is, for example,
    instead of being 16x16 and taking all the space of the
    grid, they would be 8x32 and leave blank space.
    
    The new approach uses a new function createGridItems()
    that creates a defined number of rows (divs) and a defined
    number of cells (divs) within those rows. This way,
    the grid is easier to manipulate using Flexbox.
    
    After some tinkering with flexbox, the grid is now
    working as expected.

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
-//creating the grid based on specified number
 
-const inputNumber = document.querySelector("input");
 const grid = document.querySelector(".grid");
-const startBtn = document.querySelector(".start-button")
+const inputNumber = document.querySelector("input");
+const startBtn = document.querySelector(".start-button");
 const resetBtn = document.querySelector(".reset-button");
+
+
 
 function createGridItems() {
 
-    let numOfDivs = inputNumber.value * inputNumber.value;
-    for (let i = 0; i < numOfDivs; i++) {
-        const gridSquare = document.createElement("div");
-        gridSquare.classList.add("grid-square");
-        grid.appendChild(gridSquare);
+    for (let i = 0; i < inputNumber.value; i++) {
+        const rowItem = document.createElement("div");
+        rowItem.classList.add("grid-row");
+        grid.appendChild(rowItem);
+        for (let j = 0; j < inputNumber.value; j++) {
+            const cellItem = document.createElement("div");
+            cellItem.classList.add("grid-square");
+            rowItem.appendChild(cellItem);
+        }
     }
-
 }
+
+// function createGridItems() {
+
+//     let numOfDivs = inputNumber.value * inputNumber.value;
+//     for (let i = 0; i < numOfDivs; i++) {
+//         const gridSquare = document.createElement("div");
+//         gridSquare.classList.add("grid-square");
+//         grid.appendChild(gridSquare);
+//     }
+
+// }
 
 
 //EVENT LISTENERS
+
+
+
 startBtn.addEventListener("click", () => {
     if (inputNumber.value > 100) {
         alert("Invalid Input. Please change the number!");
@@ -27,11 +45,13 @@ startBtn.addEventListener("click", () => {
     }
 })
 
+
+
 resetBtn.addEventListener("click", () => {
-    //pending to add functionality that resets the grid
     while (grid.firstChild) {
         grid.removeChild(grid.firstChild);
     }
     startBtn.disabled = false;
 })
 
+console.log(inputNumber.value)
